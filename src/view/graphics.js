1f64c12e0b2ec241ff15eb23f0cd7aeb0a4d6c89
William Dumont <william.dumont@isep.fr>
2019-05-08 21:41:48 +0200

    turn fishes to blue for Hanna

--- src/view/graphics.js
+++ src/view/graphics.js
@@ -2,7 +2,7 @@ const FISH_RADIUS = 15;
 const FRUIT_RADIUS = 5;
 
 const FRUIT_TEXTURE = PIXI.Texture.from('assets/fruit.png');
-const FISH_TEXTURE = PIXI.Texture.from('assets/fish.png');
+const FISH_TEXTURE = PIXI.Texture.from('assets/fish_blue.png');
 
 function getRoundSpriteTexture(texture, radius, layer) {
     const sprite = new PIXI.Sprite(texture);
